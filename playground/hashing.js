9a745c65d6a9f3bfc8d1e2a86c4d0f8d24a049ba
billtlee <wlee3@hotmail.com>
2018-05-17 12:02:38 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -23,16 +23,32 @@
 //   console.log('Data was changed. Do not trust!');
 // }
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
 
-var token = jwt.sign(data, '123abc');
+// var token = jwt.sign(data, '123abc');
 
-console.log(token);
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
+// var decoded = jwt.verify(token, '123abc');
 
-console.log('decoded: ', decoded);
+// console.log('decoded: ', decoded);
+
+const bcrypt = require('bcryptjs');
+
+var password = '123abc';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$z4/idzDYHVCJOznx7mm6HuAB7uDF/Lldn95aZw57V0Afeh6q0HfE2';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
