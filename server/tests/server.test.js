85d2bf4f4dd6e73e85a2b6fa9dc5f54d18aa39d5
billtlee <wlee3@hotmail.com>
2018-05-20 22:33:57 +0800

    Update server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -186,7 +186,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
-        expect(typeof(res.body.todo.completedAt)).toBe('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done)
   });
